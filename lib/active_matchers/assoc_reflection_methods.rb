40cfd3e74045dcca4a4c14a29404d21f8df48aa7
Pat Allan <pat@freelancing-gods.com>
2008-04-21 20:00:47 +1000

    Fixing up code so it can actually be used.

--- lib/active_matchers/assoc_reflection_methods.rb
+++ lib/active_matchers/assoc_reflection_methods.rb
@@ -1,10 +1,8 @@
-module ActiveMatchers
-  module AssociationReflectionMethods
-    def to_hash
-      {
-        :macro => @macro,
-        :options => @options
-      }
-    end
+module AssociationReflectionMethods
+  def to_hash
+    {
+      :macro => @macro,
+      :options => @options
+    }
   end
 end
